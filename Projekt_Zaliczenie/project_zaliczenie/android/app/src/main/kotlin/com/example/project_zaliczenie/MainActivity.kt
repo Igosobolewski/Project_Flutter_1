50472fe9a6e6df4bf2b9d8cd443a5281b812e34c
Igosobolewski <igosobolewski@gmail.com>
2023-10-01 22:30:47 +0200

    Commit1

---
+++ Projekt_Zaliczenie/project_zaliczenie/android/app/src/main/kotlin/com/example/project_zaliczenie/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.project_zaliczenie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Projekt_Zaliczenie/project_zaliczenie/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFf9f9f9),
+      appBar: AppBar(
+        backgroundColor: Color(0xFFf9f9f9),
+        elevation: 0.0,
+        title: const Text("Aplikacja Pogodofwa", style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu),
+          color: Colors.black,
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+      ),
+    );
+  }
+}
